cc7b3353223175e85771cc8ab789ca7442f90476
iashurkov <iashurkov@uma.tech>
2022-11-07 19:22:47 +0300

    Initial Commit

---
+++ FruitsApp_SwiftUI/FruitsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp_SwiftUIApp.swift
+//  FruitsApp_SwiftUI
+//
+//  Created by Igor Ashurkov on 07.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
